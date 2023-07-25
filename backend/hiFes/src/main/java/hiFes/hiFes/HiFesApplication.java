6cc2e9159b5465280c7029e094e41c3828747562
haeum <garlic820@gmail.com>
2023-07-25 15:31:57 +0900

    chore : server setup

---
+++ backend/hiFes/src/main/java/hiFes/hiFes/HiFesApplication.java
@@ -0,0 +1,13 @@
+package hiFes.hiFes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiFesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiFesApplication.class, args);
+	}
+
+}
---
+++ backend/hiFes/src/test/java/hiFes/hiFes/HiFesApplicationTests.java
@@ -0,0 +1,13 @@
+package hiFes.hiFes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiFesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
