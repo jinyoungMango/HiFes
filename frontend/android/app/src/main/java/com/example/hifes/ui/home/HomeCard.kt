95bf8463c6963fec8765fdec96715dd7be041eb3
김진영 <>
2023-07-28 08:55:20 +0900

    feat : 홈 화면에 필요한 요소 추가
    - Card, LazyColumn 등 홈 화면에 필요한 구성 요소 추가
    #2

--- frontend/android/app/src/main/java/com/example/hifes/ui/home/HomeCard.kt
+++ frontend/android/app/src/main/java/com/example/hifes/ui/home/HomeCard.kt
@@ -1,61 +1,89 @@
 package com.example.hifes.ui.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.hifes.R
 
-@Preview
+@Preview(backgroundColor = 0xFFFFFFFF)
 @Composable
 fun HomeCardPrev() {
-    Column() {
-        CustomElement()
+    Column {
+        HomeGreeting("혹시 이 행사에 참여 중이신가요?")
+        HomeFestivalImage(R.drawable.ic_launcher_background)
         HomeCard(
             title = "대구치맥페스티벌",
             content = "content"
         )
+        HomeMiddleText("이 근처에서 축제 열리는 중!")
+        HomeCardList()
     }
 
 }
 
 @Composable
-fun CustomElement() {
+fun HomeGreeting(message: String) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .height(40.dp)
             .background(Color(0xFFF11A7B))
-            .padding(8.dp)
+            .padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "혹시 이 축제에 참여하고 계신가요?",
             color = Color.White,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            text = message
         )
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeCard(title: String, content: String) {
-    ElevatedCard(
-        onClick = { /* Do something */ },
+fun HomeFestivalImage(imageResId: Int) {
+    Image(
+        painter = painterResource(id = imageResId),
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
         modifier = Modifier
             .fillMaxWidth()
-            .height(220.dp)
+            .height(200.dp)
+    )
+}
+
+@Composable
+fun HomeCard(title: String, content: String) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(220.dp),
+        color = Color.White,
+        shape = RoundedCornerShape(bottomStart = 16.dp, bottomEnd = 16.dp),
+        shadowElevation = 8.dp
     ) {
         Column(
             modifier = Modifier
@@ -69,5 +97,83 @@ fun HomeCard(title: String, content: String) {
         }
     }
 }
+
+@Composable
+fun HomeMiddleText(message: String) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = message,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp
+            )
+        }
+    }
+}
+
+@Composable
+fun HomeCardWithImage(title: String, content: String, imageRes: Int) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        shape = RoundedCornerShape(12.dp),
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.padding(12.dp)
+        ) {
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = "Card Image",
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(12.dp))
+            )
+            Column {
+                Text(
+                    text = title,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+                Text(
+                    text = content,
+                    fontSize = 18.sp,
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+            }
+
+        }
+    }
+}
+
+@Composable
+fun HomeCardList() {
+    LazyColumn {
+        items(5) { index ->
+            HomeCardWithImage(
+                title = "Festival $index",
+                content = "content",
+                imageRes = R.drawable.ic_launcher_background
+            )
+        }
+    }
+}
+
+
 
 
