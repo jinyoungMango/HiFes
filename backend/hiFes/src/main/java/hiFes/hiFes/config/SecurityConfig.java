e3de37d84d273372541bf027090ce10a46a9295f
SonJunBae <thswnsqo99@gmail.com>
2023-07-28 16:47:15 +0900

    test : "게시판 소셜 로그인 확인 중" 기존 작업 계속 작업 중 # 4

--- backend/hiFes/src/main/java/hiFes/hiFes/config/SecurityConfig.java
+++ backend/hiFes/src/main/java/hiFes/hiFes/config/SecurityConfig.java
@@ -1,11 +1,25 @@
 package hiFes.hiFes.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig extends {
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults());
+
+        return http.build();
+    }
 }
