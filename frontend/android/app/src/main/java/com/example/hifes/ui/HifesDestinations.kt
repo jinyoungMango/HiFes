efa363091ac15eaaefdb2a43438e0574668120f8
DoYeon <dodoyeon0425@gmail.com>
2023-07-30 00:16:53 +0900

    refactor : HifesNavigation 파일명 변경
    -파일명 혼돈 방지를 위해 HifesNavigation에서 HifesDestination으로 파일명 변경
    #3

--- frontend/android/app/src/main/java/com/example/hifes/ui/HifesDestinations.kt
+++ frontend/android/app/src/main/java/com/example/hifes/ui/HifesDestinations.kt
@@ -1,8 +1,5 @@
 package com.example.hifes.ui
 
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
-
 object HifesDestinations {
     const val LOGIN_ROUTE = "login"
     const val LOGIN_DETAIL_ROUTE = "login_detail"
