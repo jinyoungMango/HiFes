112284d7e9179b6acb2ebefa8aff5d3f3d77be00
DoYeon <dodoyeon0425@gmail.com>
2023-07-29 18:29:30 +0900

    feat : 네비게이션을 위한 요소 추가
    -App이 container를 가지도록 수정
    -MainActivity에서 HifesApp컴포저블을 보여주도록 MainActivity수정
    -Repository들을 가지는 AppContainerImpl추가
    -HifesTheme를 가지는 HifesApp추가
    -값에 따라 화면을 변경하는 HifesNavGraph추가
    -어떤 화면으로 갈지 destination을 저장하는 object클래스 HifesDestinations 추가
    #3

---
+++ frontend/android/app/src/main/java/com/example/hifes/ui/HifesNavGraph.kt
@@ -0,0 +1,35 @@
+package com.example.hifes.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.hifes.data.AppContainer
+import com.example.hifes.ui.login.LoginDetailScreen
+import com.example.hifes.ui.login.LoginScreen
+@Composable
+fun HifesNavGraph(
+    appContainer: AppContainer,
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = HifesDestinations.LOGIN_ROUTE
+    ) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ){
+        composable(
+            route = HifesDestinations.LOGIN_ROUTE
+        ){ navBackStackEntry ->
+            LoginScreen(navController = navController)
+        }
+        composable(
+            route = HifesDestinations.LOGIN_DETAIL_ROUTE
+        ){ navBackStackEntry ->
+            LoginDetailScreen()
+        }
+    }
+}
