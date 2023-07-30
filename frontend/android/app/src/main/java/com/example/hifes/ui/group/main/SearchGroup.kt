a1cc9f92c5c8cb8f216fdbc40c3fb24c4fc6ac35
이현근 <leehyunk6310@naver.com>
2023-07-30 10:59:50 +0900

    feat : Group 메인 화면 추가

--- frontend/android/app/src/main/java/com/example/hifes/ui/group/main/SearchGroup.kt
+++ frontend/android/app/src/main/java/com/example/hifes/ui/group/main/SearchGroup.kt
@@ -40,6 +40,8 @@ import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.hifes.ui.iconpack.MyIconPack
+import com.example.hifes.ui.iconpack.myiconpack.Search
 import com.example.hifes.ui.theme.Purple40
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -63,7 +65,7 @@ fun SearchGroup() {
         )
         IconButton(onClick = { /*TODO*/ }) {
             Icon(
-                Icons.Filled.Search,
+                imageVector = MyIconPack.Search,
                 contentDescription = "Serach",
                 tint = Color.Red,
                 modifier = Modifier.size(36.dp)
