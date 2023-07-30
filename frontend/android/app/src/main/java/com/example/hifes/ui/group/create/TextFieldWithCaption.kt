b4860522b0dfe1f2da0c71bcb06fab130b20c07e
이현근 <leehyunk6310@naver.com>
2023-07-30 10:56:45 +0900

    feat : Group 생성 화면 추가

--- frontend/android/app/src/main/java/com/example/hifes/ui/group/create/TextFieldWithCaption.kt
+++ frontend/android/app/src/main/java/com/example/hifes/ui/group/create/TextFieldWithCaption.kt
@@ -2,14 +2,27 @@ package com.example.hifes.ui.group.create
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -20,12 +33,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
 import com.example.hifes.ui.theme.Purple40
+import kotlin.math.exp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -38,28 +56,100 @@ fun TextFieldWithCaption(caption: String) {
     } else {
         BorderStroke(1.dp, Color.Black)
     }
-    Text(text = caption, fontWeight = FontWeight.Bold)
-    Spacer(modifier = Modifier.height(8.dp))
-    TextField(
-        value = text,
-        onValueChange = { newText ->
-            text = newText
-        },
-        colors = TextFieldDefaults.textFieldColors(
-            containerColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            disabledIndicatorColor = Color.Transparent
-        ),
-        modifier = Modifier
-            .fillMaxWidth()
-            .border(borderStroke, MaterialTheme.shapes.small)
-            .onFocusChanged {
-                isFocused = it.isFocused
+
+    Column() {
+
+        Text(text = caption, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = text,
+            onValueChange = { newText ->
+                text = newText
             },
-        textStyle = LocalTextStyle.current.copy(fontSize = 20.sp),
-    )
-    Spacer(modifier = Modifier.height(24.dp))
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(borderStroke, MaterialTheme.shapes.small)
+                .onFocusChanged {
+                    isFocused = it.isFocused
+                },
+            textStyle = LocalTextStyle.current.copy(fontSize = 16.sp),
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropdownWithCaption(caption: String) {
+    val options = (1..10).toList()
+
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOptionText by remember { mutableStateOf(options[0].toString()) }
+
+    var isFocused by remember { mutableStateOf(false) }
+
+    val borderStroke = if (isFocused) {
+        BorderStroke(2.dp, Purple40)    // 선택된 보더의 색 변경해야함
+    } else {
+        BorderStroke(1.dp, Color.Black)
+    }
+
+    Column() {
+
+        Text(text = caption, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(8.dp))
+        // We want to react on tap/press on TextField to show menu
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+        ) {
+            TextField(
+                // The `menuAnchor` modifier must be passed to the text field for correctness.
+                modifier = Modifier
+                    .menuAnchor()
+                    .fillMaxWidth()
+                    .border(borderStroke, MaterialTheme.shapes.small)
+                    .onFocusChanged {
+                        isFocused = it.isFocused
+                    },
+
+                readOnly = true,
+                value = selectedOptionText,
+                onValueChange = {},
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
+//            colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                ),
+                textStyle = LocalTextStyle.current.copy(fontSize = 16.sp),
+            )
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+            ) {
+                options.forEach { selectionOption ->
+                    DropdownMenuItem(
+                        text = { Text(selectionOption.toString()) },
+                        onClick = {
+                            selectedOptionText = selectionOption.toString()
+                            expanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
+                    )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+    }
 }
 
 
@@ -67,14 +157,8 @@ fun TextFieldWithCaption(caption: String) {
 @Preview
 @Composable
 fun EditPrev() {
-    val sports = mutableListOf("Basketball", "Rugby", "Football", "MMA", "Motorsport", "Snooker", "Tennis")
-
-    Column(Modifier.padding(horizontal = 8.dp)) {
-        TextFieldWithCaption(caption = "caption")
-        TextFieldWithCaption(caption = "caption")
-        TextFieldWithCaption(caption = "caption")
-        
-        }
+    TextFieldWithCaption(caption = "caption")
+    DropdownWithCaption(caption = "caption")
 }
 
 
