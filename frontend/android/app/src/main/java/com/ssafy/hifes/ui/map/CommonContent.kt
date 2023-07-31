b505253a7a1fbd0bff88c64f38d564f84b037a2c
jinyoung <kimjinzer099@gmail.com>
2023-07-31 17:25:18 +0900

    feat : 행사 상세 화면 추가
    - 행사 상세 화면 FestivalDetail 작성
    - 위 화면에서 필요한 아이콘 drawable에 추가
    - Date 타입의 데이터를 포맷팅해주는 CommonUtils 클래스 추가
    #2

--- frontend/android/app/src/main/java/com/ssafy/hifes/ui/map/CommonContent.kt
+++ frontend/android/app/src/main/java/com/ssafy/hifes/ui/map/CommonContent.kt
@@ -1,7 +1,6 @@
 package com.ssafy.hifes.ui.map
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -25,9 +24,8 @@ import androidx.compose.ui.unit.sp
 import com.ssafy.hifes.R
 
 
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
 @Composable
-fun MapPreview() {
+fun MapScreen() {
     DialogContent(
         image = R.drawable.ic_launcher_foreground,
         score = 4.0,
@@ -35,6 +33,13 @@ fun MapPreview() {
         startDate = "startDate",
         endDate = "endDate"
     )
+}
+
+
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+fun MapPreview() {
+    MapScreen()
 }
 
 @Composable
@@ -49,7 +54,6 @@ fun DialogContent(image: Int, score: Double, address: String, startDate: String,
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
             modifier = Modifier.padding(12.dp)
         ) {
             Image(
@@ -59,25 +63,31 @@ fun DialogContent(image: Int, score: Double, address: String, startDate: String,
                     .size(150.dp)
                     .clip(RoundedCornerShape(12.dp))
             )
+            Spacer(modifier = Modifier.size(12.dp))
             Column {
-                Row {
-                    Spacer(modifier = Modifier.size(12.dp))
-                    Image(
-                        painter = painterResource(id = R.drawable.icon_star),
-                        contentDescription = "star",
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.size(4.dp))
-                    Text(
-                        text = score.toString(),
-                        color = Color.Black,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 18.sp
-                    )
-                }
+                StarScore(score)
                 TextTitleWithContent(address = address, startDate = startDate, endDate = endDate)
             }
         }
+    }
+}
+
+
+@Composable
+fun StarScore(score: Double) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.icon_star),
+            contentDescription = "star",
+            modifier = Modifier.size(24.dp)
+        )
+        Spacer(modifier = Modifier.size(4.dp))
+        Text(
+            text = score.toString(),
+            color = Color.Black,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp
+        )
     }
 }
 
@@ -88,28 +98,24 @@ fun TextTitleWithContent(address: String, startDate: String, endDate: String) {
         text = "장소",
         color = Color.Black,
         fontWeight = FontWeight.Bold,
-        fontSize = 18.sp,
-        modifier = Modifier.padding(horizontal = 12.dp)
+        fontSize = 18.sp
     )
     Text(
         text = address,
         color = Color.Black,
-        fontSize = 18.sp,
-        modifier = Modifier.padding(horizontal = 12.dp)
+        fontSize = 18.sp
     )
 
     Spacer(modifier = Modifier.size(8.dp))
     Text(
-        text = "장소",
+        text = "일정",
         color = Color.Black,
         fontWeight = FontWeight.Bold,
-        fontSize = 18.sp,
-        modifier = Modifier.padding(horizontal = 12.dp)
+        fontSize = 18.sp
     )
     Text(
         text = address,
         color = Color.Black,
-        fontSize = 18.sp,
-        modifier = Modifier.padding(horizontal = 12.dp)
+        fontSize = 18.sp
     )
 }
