40c562599ea866f9e46bf773ef5fd3370a56e684
DoYeon <dodoyeon0425@gmail.com>
2023-08-02 00:25:29 +0900

    feat : 칩 눌렀을때 게시판 이동 구현
    -칩을 눌렀을때 다른 게시판의 글이 보이도록 수정함
    -게시판 리스트 화면에 칩이 있도록 하고, 칩을 눌렀을때 칩에 따라 다른 게시판의 게시글 리스트가 보이도록 함
    -칩을 눌렀을때 몇번째 칩을 눌렀는지 알 수 있도록 인덱스를 받는 함수를 ChipsSelectable의 매개변수로 받을 수 있도록 수정
    -ChipsSelectable에서 선택된 칩의 색상을 PrimaryPink로 수정
    #6

--- frontend/android/app/src/main/java/com/ssafy/hifes/ui/board/BoardScreen.kt
+++ frontend/android/app/src/main/java/com/ssafy/hifes/ui/board/BoardScreen.kt
@@ -3,44 +3,57 @@ package com.ssafy.hifes.ui.board
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Text
-import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModelProvider
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
-import com.ssafy.hifes.ui.common.top.TopWithBack
 import com.ssafy.hifes.R
+import com.ssafy.hifes.ui.common.ChipsSelectable
+import com.ssafy.hifes.ui.common.top.TopWithBack
+import com.ssafy.hifes.ui.theme.LightGrey
 
 @Composable
 fun BoardScreen(
     navController: NavController,
-    viewModel : BoardViewModel
+    viewModel: BoardViewModel
 ) {
     val postList = viewModel.postList.observeAsState()
 
-    Scaffold (
-        topBar = {TopWithBack(title = stringResource(id = R.string.board_appbar_title))}
-    ){
-        if(postList.value!=null){
-            Column(modifier = Modifier.padding(it)) {
-                Button(onClick = {viewModel.getAskPostList()}) {
-                    Text(text = "공지글 보기")
+    Scaffold(
+        topBar = { TopWithBack(title = stringResource(id = R.string.board_appbar_title)) }
+    ) {
+        if (postList.value != null) {
+            Column(
+                modifier = Modifier.padding(it),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ChipsSelectable(
+                    listOf(
+                        stringResource(id = R.string.board_chip_notification),
+                        stringResource(id = R.string.board_chip_ask),
+                        stringResource(id = R.string.board_chip_free),
+                        stringResource(id = R.string.board_chip_review)
+                    )
+                ) { index ->
+                    when (index) {
+                        0 -> viewModel.getNotificationPostList()
+                        1 -> viewModel.getAskPostList()
+                        2 -> viewModel.getFreePostList()
+                        3 -> viewModel.getReviewPostList()
+                    }
                 }
-                Button(onClick = {viewModel.getFreePostList()}) {
-                    Text(text = "자유글 보기")
-                }
-                Button(onClick = {viewModel.getReviewPostList()}) {
-                    Text(text = "리뷰글 보기")
-                }
-                LazyColumn{
-                    items(postList.value!!.size){index ->
+                Divider(color = LightGrey, thickness = 2.dp)
+
+                LazyColumn (modifier = Modifier.padding(16.dp)){
+                    items(postList.value!!.size) { index ->
                         PostItem(postData = postList.value!!.get(index), viewModel.userDataId, {})
                     }
                 }
@@ -54,7 +67,7 @@ fun BoardScreen(
 
 @Composable
 @Preview
-fun PreviewBoardScreen(){
+fun PreviewBoardScreen() {
     val homeViewModel: BoardViewModel = viewModel()
 
     BoardScreen(navController = rememberNavController(), viewModel = homeViewModel)
