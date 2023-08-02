a6f02ad864a6b04e1b86d743594217230628f7bb
DoYeon <dodoyeon0425@gmail.com>
2023-08-03 03:13:44 +0900

    feat : 댓글 작성 창 추가
    -게시글 상세 화면 하단에 붙어 다니는 댓글 작성 레이아웃 추가
    -댓글 작성 레이아웃 CommentwriteComponent추가
    -댓글 작성 창에 필요한 문자열 추가
    -댓글 작성 창에 필요한 아이콘 추가
    #6

--- frontend/android/app/src/main/java/com/ssafy/hifes/ui/board/boarddetail/BoardDetailScreen.kt
+++ frontend/android/app/src/main/java/com/ssafy/hifes/ui/board/boarddetail/BoardDetailScreen.kt
@@ -1,16 +1,28 @@
 package com.ssafy.hifes.ui.board.boarddetail
 
+import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Divider
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.boundsInRoot
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -18,27 +30,51 @@ import androidx.navigation.compose.rememberNavController
 import com.ssafy.hifes.data.model.FestivalTableDto
 import com.ssafy.hifes.data.model.PostDto
 import com.ssafy.hifes.ui.board.BoardViewModel
+import com.ssafy.hifes.ui.board.boarddetail.comment.CommentWriteComponent
 import java.text.SimpleDateFormat
 
 @Composable
 fun BoardDetailScreen(navController: NavController, viewModel: BoardViewModel) {
     val selectedPost = viewModel.selectedPost.observeAsState()
+    val localDensity = LocalDensity.current
     val scrollState = rememberScrollState()
-    Column(modifier = Modifier.verticalScroll(scrollState)) {
+    var height by remember { mutableStateOf(0.dp) }
+    if (selectedPost.value != null) {
+        Scaffold(
+            topBar = { BoardDetailTopAppBar(selectedPost.value!!, viewModel) },
+            content = { it ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .padding(it)
+                ) {
+                    Column(modifier = Modifier.verticalScroll(scrollState)) {
 
-        if (selectedPost.value != null) {
-            BoardDetailTopAppBar(selectedPost.value!!, viewModel)
-            Column(modifier = Modifier.padding(16.dp, 0.dp)) {
-                BoardDetailHead(postData = selectedPost.value!!)
-                BoardDetailBody(postData = selectedPost.value!!)
-                Spacer(modifier = Modifier.size(20.dp))
-                Divider()
-                Spacer(modifier = Modifier.size(20.dp))
-                BoardDetailComments(viewModel = viewModel)
+                        Column(modifier = Modifier.padding(16.dp, 0.dp)) {
+                            BoardDetailHead(postData = selectedPost.value!!)
+                            BoardDetailBody(postData = selectedPost.value!!)
+                            Spacer(modifier = Modifier.size(20.dp))
+                            Divider()
+                            Spacer(modifier = Modifier.size(20.dp))
+                            BoardDetailComments(viewModel = viewModel)
+                        }
+                        Spacer(modifier = Modifier.size(40.dp))
+                    }
+                    Box(modifier = Modifier.align(Alignment.BottomCenter)) {
+                        CommentWriteComponent(
+                            viewModel = viewModel,
+                            onGloballyPositioned = { layoutCoordinates ->
+                                height = with(localDensity){
+                                    layoutCoordinates.size.height.toDp()
+                                }
+                            }
+                        )
+                    }
+                }
             }
-        }
+        )
     }
-
 }
 
 @Composable
--- frontend/android/app/src/main/java/com/ssafy/hifes/ui/board/boarddetail/comment/CommentWriteComponent.kt
+++ frontend/android/app/src/main/java/com/ssafy/hifes/ui/board/boarddetail/comment/CommentWriteComponent.kt
@@ -1,7 +1,75 @@
 package com.ssafy.hifes.ui.board.boarddetail.comment
 
-import com.ssafy.hifes.data.model.CommentDto
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ssafy.hifes.R
+import com.ssafy.hifes.ui.board.BoardViewModel
+import com.ssafy.hifes.ui.iconpack.MyIconPack
+import com.ssafy.hifes.ui.iconpack.myiconpack.Writecomment
+import com.ssafy.hifes.ui.theme.PrimaryPink
 
-class CommentWriteComponent(commentData : CommentDto) {
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CommentWriteComponent(
+    viewModel: BoardViewModel,
+    onGloballyPositioned:(LayoutCoordinates) -> Unit,
+) {
+    var text by remember { mutableStateOf("") }
 
+    Column (
+        modifier = Modifier
+            .background(color = Color.White)
+            .onGloballyPositioned(onGloballyPositioned)
+    ){
+        Row (verticalAlignment = Alignment.CenterVertically){
+            Spacer(modifier = Modifier.size(20.dp))
+            OutlinedTextField(
+                value = text,
+                onValueChange = { text = it },
+                colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = PrimaryPink),
+                placeholder = { Text(text = stringResource(id = R.string.board_detail_write_comment_hint)) },
+                shape = RoundedCornerShape(14.dp)
+            )
+            Spacer(modifier = Modifier.size(10.dp))
+            Icon(
+                imageVector = MyIconPack.Writecomment,
+                contentDescription = "댓글 쓰기",
+                tint = Color.Unspecified,
+                modifier = Modifier.size(34.dp)
+            )
+            Spacer(modifier = Modifier.size(20.dp))
+        }
+        Spacer(modifier = Modifier.size(10.dp))
+    }
+
+}
+
+@Composable
+@Preview
+fun PreviewCommentWriteComponent() {
+  //  CommentWriteComponent(viewModel = BoardViewModel())
 }
