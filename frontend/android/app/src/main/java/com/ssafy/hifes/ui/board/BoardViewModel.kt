a5c459ab37d2b900fbb6fd694879abb508c3eca8
dodoyeon0425@gmail.com <dodoyeon0425@gmail.com>
2023-08-02 13:09:36 +0900

    style : 더미데이터 값 변경
    -viewModel에 들어가는 게시글 더미 데이터 값 변경
    #6

--- frontend/android/app/src/main/java/com/ssafy/hifes/ui/board/BoardViewModel.kt
+++ frontend/android/app/src/main/java/com/ssafy/hifes/ui/board/BoardViewModel.kt
@@ -31,7 +31,7 @@ class BoardViewModel : ViewModel() {
             add(PostDto(1, 1, 1, 1, "공지1", "내용", "notification", null, null, "글쓴이", postTestDate, postTestDate, 1, null, null))
             add(PostDto(1, 1, 1, 1, "공지2", "내용", "notification", null, null, "글쓴이", postTestDate, postTestDate, 1, null, null))
             add(PostDto(1, 1, 1, 1, "공지3", "내용", "notification", null, null, "글쓴이", postTestDate, postTestDate, 1, null, null))
-            add(PostDto(1, 1, 1, 2, "공지4", "내용", "notification", null, null, "글쓴이", postTestDate, postTestDate, 1, "https://fastly.picsum.photos/id/296/200/200.jpg?hmac=y-H33xJ0Tpm9muoZO3ZMb5kXpNPG1mptQ9HBmpjCc8A", null))
+            add(PostDto(1, 1, 1, 2, "공지4", "내용", "notification", null, null, "글쓴이", postTestDate, postTestDate, 1, "https://picsum.photos/600", null))
             add(PostDto(1, 1, 1, 2, "공지5", "내용", "notification", null, null, "글쓴이", postTestDate, postTestDate, 1, null, null))
         }
         _postList.postValue(postListDummyData)
@@ -41,9 +41,9 @@ class BoardViewModel : ViewModel() {
         var postListDummyData = mutableListOf<PostDto>()
         postListDummyData.apply {
             add(PostDto(1, 1, 1, 1, "내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글내문의1 비밀글", "내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1내용1", "ask", true, null, "글쓴이", postTestDate, postTestDate, 1, "https://fastly.picsum.photos/id/296/200/200.jpg?hmac=y-H33xJ0Tpm9muoZO3ZMb5kXpNPG1mptQ9HBmpjCc8A", null))
-            add(PostDto(1, 1, 1, 1, "내문의2 공개글", "내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2", "ask", false, null, "글쓴이", postTestDate, postTestDate, 1, "https://fastly.picsum.photos/id/296/200/200.jpg?hmac=y-H33xJ0Tpm9muoZO3ZMb5kXpNPG1mptQ9HBmpjCc8A", null))
-            add(PostDto(1, 1, 1, 2, "남의 문의1 비밀글", "내용3", "ask", true, null, "글쓴이", postTestDate, postTestDate, 1, "https://fastly.picsum.photos/id/296/200/200.jpg?hmac=y-H33xJ0Tpm9muoZO3ZMb5kXpNPG1mptQ9HBmpjCc8A", null))
-            add(PostDto(1, 1, 1, 2, "남의 문의2 공개글", "내용4", "ask", false, null, "글쓴이", postTestDate, postTestDate, 1, "https://fastly.picsum.photos/id/296/200/200.jpg?hmac=y-H33xJ0Tpm9muoZO3ZMb5kXpNPG1mptQ9HBmpjCc8A", null))
+            add(PostDto(1, 1, 1, 1, "내문의2 공개글", "내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2내용2", "ask", false, null, "글쓴이", postTestDate, postTestDate, 1, "https://picsum.photos/1000", null))
+            add(PostDto(1, 1, 1, 2, "남의 문의1 비밀글", "내용3", "ask", true, null, "글쓴이", postTestDate, postTestDate, 1, "https://picsum.photos/600", null))
+            add(PostDto(1, 1, 1, 2, "남의 문의2 공개글", "내용4", "ask", false, null, "글쓴이", postTestDate, postTestDate, 1, "https://picsum.photos/1000", null))
         }
         _postList.postValue(postListDummyData)
     }
@@ -52,9 +52,9 @@ class BoardViewModel : ViewModel() {
         var postListDummyData = mutableListOf<PostDto>()
         postListDummyData.apply {
             add(PostDto(1, 1, 1, 1, "자유글1", "내용1", "free", null, null, "글쓴이", postTestDate, postTestDate, 1, null, null))
-            add(PostDto(1, 1, 1, 1, "자유글2", "내용2", "free", null, null, "글쓴이", postTestDate, postTestDate, 1, "https://fastly.picsum.photos/id/296/200/200.jpg?hmac=y-H33xJ0Tpm9muoZO3ZMb5kXpNPG1mptQ9HBmpjCc8A", null))
+            add(PostDto(1, 1, 1, 1, "자유글2", "내용2", "free", null, null, "글쓴이", postTestDate, postTestDate, 1, "https://picsum.photos/1600", null))
             add(PostDto(1, 1, 1, 1, "자유글3", "내용3", "free", null, null, "글쓴이", postTestDate, postTestDate, 1, null, null))
-            add(PostDto(1, 1, 1, 2, "자유글4", "내용4", "free", null, null, "글쓴이", postTestDate, postTestDate, 1, "https://fastly.picsum.photos/id/296/200/200.jpg?hmac=y-H33xJ0Tpm9muoZO3ZMb5kXpNPG1mptQ9HBmpjCc8A", null))
+            add(PostDto(1, 1, 1, 2, "자유글4", "내용4", "free", null, null, "글쓴이", postTestDate, postTestDate, 1, "https://picsum.photos/2000", null))
             add(PostDto(1, 1, 1, 2, "자유글5", "내용5", "free", null, null, "글쓴이", postTestDate, postTestDate, 1, null, null))
         }
         _postList.postValue(postListDummyData)
@@ -64,7 +64,7 @@ class BoardViewModel : ViewModel() {
         var postListDummyData = mutableListOf<PostDto>()
         postListDummyData.apply {
             add(PostDto(1, 1, 1, 1, "리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1리뷰1", "내용내용내용내용내용내용내용내용내용내용내용", "review", null, null, "글쓴이", postTestDate, postTestDate, 1, null, 0f))
-            add(PostDto(1, 1, 1, 1, "리뷰2", "내용", "review", null, null, "글쓴이", postTestDate, postTestDate, 1, "https://fastly.picsum.photos/id/296/200/200.jpg?hmac=y-H33xJ0Tpm9muoZO3ZMb5kXpNPG1mptQ9HBmpjCc8A", 3f))
+            add(PostDto(1, 1, 1, 1, "리뷰2", "내용", "review", null, null, "글쓴이", postTestDate, postTestDate, 1, "https://picsum.photos/1000", 3f))
             add(PostDto(1, 1, 1, 1, "리뷰3", "내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용", "review", null, null, "글쓴이", postTestDate, postTestDate, 1, "https://fastly.picsum.photos/id/296/200/200.jpg?hmac=y-H33xJ0Tpm9muoZO3ZMb5kXpNPG1mptQ9HBmpjCc8A", 5f))
             add(PostDto(1, 1, 1, 2, "리뷰4", "내용", "review", null, null, "글쓴이", postTestDate, postTestDate, 1, null, 3.5f))
             add(PostDto(1, 1, 1, 2, "리뷰5", "내용", "review", null, null, "글쓴이", postTestDate, postTestDate, 1, null, 2.5f))
