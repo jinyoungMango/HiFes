b39b17f3266421eb8a273e69ac462a637745d231
SonJunBae <thswnsqo99@gmail.com>
2023-08-11 14:38:45 +0900

    fix: swagger annotation created

--- backend/hiFes/src/main/java/hiFes/hiFes/controller/CommentController.java
+++ backend/hiFes/src/main/java/hiFes/hiFes/controller/CommentController.java
@@ -4,6 +4,7 @@ import hiFes.hiFes.domain.Comment;
 import hiFes.hiFes.dto.commentDto.CommentCreateDto;
 import hiFes.hiFes.dto.commentDto.CommentUpdateDto;
 import hiFes.hiFes.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,12 +25,14 @@ public class CommentController {
 //    }
 
     @PostMapping("/create")
+    @Operation(summary = "댓글 생성, 필요 값 postId(Long), content(Long), parentId(Long)")
     public ResponseEntity<?> createComment(@RequestBody CommentCreateDto requestDto) {
         commentService.create(requestDto);
         return ResponseEntity.ok(requestDto);
     }
 
     @PostMapping("/create/re")
+    @Operation(summary = "대댓글 생성, 필요 값 postId(Long), content(Long), parentId(Long)")
     public ResponseEntity<?> createReComment(@RequestBody CommentCreateDto requestDto) {
         commentService.create(requestDto);
         return ResponseEntity.ok(requestDto);
@@ -41,11 +44,13 @@ public class CommentController {
 //    }
 
     @PutMapping("/update/{id}")
+    @Operation(summary = "댓글 수정, 필요 값 commentId(Long), content(Long)")
     public Long update(@PathVariable Long id, @RequestBody CommentUpdateDto requestDto) {
         return commentService.update(id, requestDto);
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "댓글 삭제, 필요 값 commentId(Long)")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         commentService.delete(id);
         Map<String, String> response = new HashMap<>();
