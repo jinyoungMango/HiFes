4963d81ee3a6637dea4d69e2314e48283647c88f
DoYeon <dodoyeon0425@gmail.com>
2023-08-12 15:01:36 +0900

    feat : 알림 오레오 이상 버전 대응
    -오레오 이상 버전인 경우 채널 생성 필요, 이에 오레오 버전 이상인 경우 채널을 생성하도록 수정
    #11

--- frontend/android/app/src/main/java/com/ssafy/hifes/data/service/FirebaseMessageService.kt
+++ frontend/android/app/src/main/java/com/ssafy/hifes/data/service/FirebaseMessageService.kt
@@ -1,21 +1,28 @@
 package com.ssafy.hifes.data.service
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.ssafy.hifes.R
 import com.ssafy.hifes.ui.main.MainActivity
 import java.util.UUID
 
 
 private const val TAG = "FirebaseMessageService_싸피"
-class FirebaseMessageService: FirebaseMessagingService() {
+
+class FirebaseMessageService : FirebaseMessagingService() {
+    val CHANNEL_ID = "1"
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
@@ -24,17 +31,16 @@ class FirebaseMessageService: FirebaseMessagingService() {
     }
 
     // Foreground, Background 모두 처리하기 위해서는 data에 값을 담아서 넘긴다.
-    //https://firebase.google.com/docs/cloud-messaging/android/receive
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         var messageTitle = ""
         var messageContent = ""
 
-        if(remoteMessage.notification != null){ // notification이 있는 경우 foreground처리
+        if (remoteMessage.notification != null) { // notification이 있는 경우 foreground처리
             //foreground
-            messageTitle= remoteMessage.notification!!.title.toString()
+            messageTitle = remoteMessage.notification!!.title.toString()
             messageContent = remoteMessage.notification!!.body.toString()
 
-        }else{  // background 에 있을경우 혹은 foreground에 있을경우 두 경우 모두
+        } else {  // background 에 있을경우 혹은 foreground에 있을경우 두 경우 모두
             var data = remoteMessage.data
             Log.d(TAG, "data.message: ${data}")
             Log.d(TAG, "data.message: ${data.get("title")}")
@@ -48,10 +54,26 @@ class FirebaseMessageService: FirebaseMessagingService() {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }
 
-        val mainPendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, mainIntent, PendingIntent.FLAG_IMMUTABLE)
+        val mainPendingIntent: PendingIntent =
+            PendingIntent.getActivity(this, 0, mainIntent, PendingIntent.FLAG_IMMUTABLE)
 
-        val builder1 = NotificationCompat.Builder(this, "1")
-            .setSmallIcon(android.R.drawable.ic_dialog_info)
+        var builder: NotificationCompat.Builder
+        var notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                "hifes notification",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+            builder = NotificationCompat.Builder(this@FirebaseMessageService, CHANNEL_ID)
+        } else {
+            builder = NotificationCompat.Builder(this@FirebaseMessageService)
+        }
+
+        builder.setSmallIcon(R.drawable.logo_background_hifes)
             .setContentTitle(messageTitle)
             .setContentText(messageContent)
             .setAutoCancel(true)
@@ -65,7 +87,7 @@ class FirebaseMessageService: FirebaseMessagingService() {
             ) {
                 return
             }
-            notify(101, builder1.build())
+            notify(101, builder.build())
         }
     }
 
