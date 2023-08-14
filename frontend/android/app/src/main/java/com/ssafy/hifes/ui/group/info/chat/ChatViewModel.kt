e75455035a1256bffb0f69373d610b2e8a3e1222
jinyoung <kimjinzer099@gmail.com>
2023-08-14 16:42:42 +0900

    style : 사용하지 않는 코드 제거
    - 사용하지 않는 코드, 임포트 제거
    - 코드 자동정렬
    #14

--- frontend/android/app/src/main/java/com/ssafy/hifes/ui/group/info/chat/ChatViewModel.kt
+++ frontend/android/app/src/main/java/com/ssafy/hifes/ui/group/info/chat/ChatViewModel.kt
@@ -2,28 +2,22 @@ package com.ssafy.hifes.ui.group.info.chat
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import com.google.gson.Gson
 import com.ssafy.hifes.data.model.MessageData
 import com.ssafy.hifes.data.model.UserData
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.launch
 
 private const val TAG = "ChatViewModel_하이페스"
 
 class ChatViewModel : ViewModel() {
-    val gson = Gson()
     private val firebaseDB = Firebase.database
 
     var chatMessages = MutableStateFlow<List<MessageData>>(emptyList())
 
-    //    var chatData = MutableStateFlow<ChatData?>(null)
-    var userId = 29
 
     // 채팅방 들어가자마자 조회 - 한번도 채팅하지 않은 경우(채팅방이 생성되어있지 않은 경우) 조회불가
     fun enterChatRoom(groupId: String) {
@@ -32,10 +26,6 @@ class ChatViewModel : ViewModel() {
         firebaseDB.reference.child("chat").child(groupId).get()
             .addOnSuccessListener {
                 Log.d("채팅방 정보", it.value.toString())
-//                // 데이터는 hashMap 형태로 오기때문에 객체 형태로 변환해줘야함
-//                Log.d(TAG, "enterChatRoom: ")
-//                val messageData = gson.fromJson(it.value.toString(), MessageData::class.java)
-//                Log.d(TAG, "enterChatRoom: $messageData")
             }
             .addOnFailureListener {
                 Log.d("채팅룸 정보 가져오기 실패", it.toString())
@@ -80,28 +70,7 @@ class ChatViewModel : ViewModel() {
             .addValueEventListener(chatListener)
     }
 
-    // 메세지 보내기
-    fun newMessageTest(chatId: String, messageData: MessageData) {
-        if (chatMessages.value.isEmpty()) {
-            chatMessages.value = listOf(messageData)
-            // 첫 메세지일때 채팅방 생성 - 채팅룸이 생성되는 시점
-//            newChatRoom(chatId, userId, chatMessages.value)
-        } else {
-            chatMessages.value += messageData
-            firebaseDB.reference.child("chat").child(chatId).child("messages")
-                .setValue(chatMessages.value)
-                .addOnSuccessListener {
-                    Log.d("newChatRoomSuccess", "메세지 보내기 성공")
-                }
-                .addOnFailureListener {
-                    Log.d("메세지 보내기 실패", it.toString())
-                }
-        }
-    }
-
     fun sendNewMessage(groupId: String, messageData: MessageData) {
-
-        Log.d(TAG, "testNewMessage: chatMeesages -> ${chatMessages.value}")
         chatMessages.value += messageData
         Log.d(TAG, "testNewMessage: chatMeesages -> ${chatMessages.value}")
 
@@ -114,27 +83,5 @@ class ChatViewModel : ViewModel() {
                 Log.d("메세지 보내기 실패", it.toString())
             }
 
-    }
-
-    // 채팅룸 생성
-    private fun newChatRoom(
-        groupId: String,
-        message: List<MessageData>
-    ) {
-        viewModelScope.launch {
-//            val chatData = ChatData(chatId, message)
-
-            firebaseDB.reference.child("chat").child(groupId).setValue(message)
-                .addOnSuccessListener {
-                    Log.d("newChatRoomSuccess", "채팅룸 생성 완료")
-                    // 생성시 채팅 listener 재호출
-                    enterChatRoom(groupId)
-                }
-                .addOnFailureListener {
-                    Log.d("채팅룸 생성 실패", it.toString())
-                }
-
-
-        }
     }
 }
