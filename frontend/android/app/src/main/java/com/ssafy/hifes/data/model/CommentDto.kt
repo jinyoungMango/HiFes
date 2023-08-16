a368b1be936c78b5d56a15160ec1046306941fb0
DoYeon <dodoyeon0425@gmail.com>
2023-08-16 23:19:57 +0900

    feat  : 게시글 상세 조회를 위한 dto 수정
    -게시글 상세 조회를 위한 dto수정
    #16

--- frontend/android/app/src/main/java/com/ssafy/hifes/data/model/CommentDto.kt
+++ frontend/android/app/src/main/java/com/ssafy/hifes/data/model/CommentDto.kt
@@ -3,14 +3,11 @@ package com.ssafy.hifes.data.model
 import java.sql.Date
 
 data class CommentDto(
-    var commentId: Int,
-    var normalUserId: Int,
-    var postId: Int,
-    var festivalId: Int,
-    var hostId: Int,
-    var comment: String,
-    var createdAt: Date,
-    var createdBy: String,
-    var updatedAt: Date,
-    var depth: Boolean
+    var id: Int,
+    var content: String,
+    var createdBy: Int,
+    var createdAt: DateTime,
+    var postId: Int?,
+    var childComments: List<CommentDto>,
+    var writer: String,
 )
--- frontend/android/app/src/main/java/com/ssafy/hifes/data/model/PostDto.kt
+++ frontend/android/app/src/main/java/com/ssafy/hifes/data/model/PostDto.kt
@@ -16,3 +16,19 @@ data class PostDto(
     var rating: Float?,
     var commentsCount: Int = 0
 )
+
+data class PostDetailDto(
+    var id: Int,
+    var createdBy: Int,
+    var writer: String,
+    var title: String,
+    var content: String,
+    var postType: String,
+    var isHidden: Boolean?,
+    var imagePath: String?,
+    var organizedFestivalId: Int,
+    var views: Int,
+    var rating: Float?,
+    var createdAt: DateTime,
+    var topLevelComments: List<CommentDto>
+)
