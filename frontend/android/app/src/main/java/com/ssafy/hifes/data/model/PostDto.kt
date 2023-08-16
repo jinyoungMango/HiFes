38df86e424778818ef9c17559a29aa986b8d90bc
DoYeon <dodoyeon0425@gmail.com>
2023-08-16 15:44:13 +0900

    feat : 게시글 조회를 위한 dto 수정
    -게시글 리스트 조회를 위한 dto 수정
    #16

--- frontend/android/app/src/main/java/com/ssafy/hifes/data/model/PostDto.kt
+++ frontend/android/app/src/main/java/com/ssafy/hifes/data/model/PostDto.kt
@@ -1,21 +1,18 @@
 package com.ssafy.hifes.data.model
 
-import java.sql.Date
-
 data class PostDto(
-    var postId: Int,
-    var festivalId: Int,
-    var hostId: Int,
-    var normalUserId: Int,
+    var id: Int,
+    var createdBy: Int,
+    var writer: String,
     var title: String,
-    var content: String,
     var postType: String,
-    var hidden: Boolean?,
+    var organizedFestivalId: Int,
+    var views: Int,
+    var createdAt: DateTime,
+    var content: String="",
+    var isHidden: Boolean?,
     var hideReason: String?,
-    var createdBy: String,
-    var createdAt: Date,
-    var updatedAt: Date,
-    var seenTimes: Int,
-    var picture: String?,
-    var rating: Float?
+    var imagePath: String?,
+    var rating: Float?,
+    var commentsCount: Int = 0
 )
